8c8e46b97d73f422d3cf4299e8477255cd9a29a7
Alexander Netter <xan@AlexandersMBP2.lan1>
2019-01-29 10:42:47 -0500

    clicky

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,11 @@
+//sets up the reusable Footer component
+import React from "react";
+import "./Footer.css";
+
+const Footer = () => (
+	<header className = "footer">
+		<p>Xan Netter</p>
+	</header>
+);
+
+export default Footer;
---
+++ src/components/Navbar/Navbar.js
@@ -0,0 +1,19 @@
+
+import React, { Component } from "react";
+import "./Navbar.css";
+
+class Navbar extends Component {
+  render() {
+    return (
+      <nav className="navbar navbar-default navbar-fixed-top">
+         <ul>
+          <li className="itemLeft">Memory Test</li>
+          <li className="itemCenter"></li>
+          <li className="itemRight">Score: {this.props.score}</li>
+        </ul>
+      </nav>
+    );
+  }
+}
+
+export default Navbar;
